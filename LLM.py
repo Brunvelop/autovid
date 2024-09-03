3678011080d1c5d5d156bd8f8a8ffbaf99cd8684
Brunvelop <brunvelop@gmail.com>
2024-09-03 19:19:27 +0200

    Refactor LLM

--- LLM.py
+++ LLM.py
@@ -2,67 +2,43 @@ import os
 from typing import Union
 from pathlib import Path
 from dotenv import load_dotenv
+from abc import ABC, abstractmethod
 
+import torch
 from langchain_community.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage, SystemMessage
 from langchain_community.llms import HuggingFacePipeline
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline, BitsAndBytesConfig
 
 from definitions import LLMModels
 
-class LLM():
+class LLM(ABC):
     def __init__(
         self, 
-        model_id: LLMModels = LLMModels.GPT4o,
         cache_dir: Path = Path('./models'), 
         low_vram: bool = False, 
         llm_config: dict = {}
     ):
         load_dotenv()
-        self.model_id = model_id.value
         self.cache_dir = cache_dir
-        self.chat_model = self._load_chat_model(model_id, low_vram, llm_config)
+        self.chat_model = self._load_chat_model(low_vram, llm_config)
 
-    def _load_chat_model(
-            self,
-            model_id: LLMModels,
-            low_vram: bool,
-            llm_config: dict,
-        ) -> Union[ChatOpenAI, HuggingFacePipeline]:
-        if model_id == LLMModels.GPT4o or model_id == LLMModels.GPT4oMini:
-            return ChatOpenAI(
-                openai_api_key=os.getenv("OPENAI_API_KEY"),
-                model_name=model_id.value,
-                temperature=llm_config.get('temperature', 0.8),
-            )
-        elif model_id == LLMModels.INTERNLM7b4b or model_id ==LLMModels.INTERNLM:
-            print("Tring to load LLM:", model_id.value)
-            tokenizer = AutoTokenizer.from_pretrained(model_id.value, trust_remote_code=True, cache_dir=self.cache_dir)
-            model = AutoModelForCausalLM.from_pretrained(
-                model_id.value,
-                device_map="auto",
-                # device=0,  # -1 for CPU
-                trust_remote_code=True,
-                load_in_4bit=True,
-                cache_dir=self.cache_dir,
-            )
-            pipe = pipeline(model_id.value, model=model, tokenizer=tokenizer)
-            print("Loaded!:", model_id.value)
-            return HuggingFacePipeline(pipeline=pipe)
-        elif model_id == LLMModels.GEMMA2B:
-            print("Trying to load LLM:", model_id.value)
-            tokenizer = AutoTokenizer.from_pretrained(model_id.value, cache_dir=self.cache_dir)
-            model = AutoModelForCausalLM.from_pretrained(
-                model_id.value,
-                device_map="auto",
-                trust_remote_code=True,
-                revision="float16",
-                cache_dir=self.cache_dir,
-            )
-            pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
-            print("Loaded!:", model_id.value)
-            return HuggingFacePipeline(pipeline=pipe)
-    
+    @abstractmethod
+    def generate_text(
+            self, 
+            system_memory: str = '',
+            system_prompt: str = '',
+            system_examples: str = '', 
+            human_prompt: str = '',
+            output_format: str = ''
+        ) -> str:
+        pass
+
+    @abstractmethod
+    def _load_chat_model(self, low_vram: bool, llm_config: dict) -> Union[ChatOpenAI, HuggingFacePipeline]:
+        pass
+
+class OpenAILLM(LLM, ABC):
     def generate_text(
             self, 
             system_memory: str = '',
@@ -76,11 +52,25 @@ class LLM():
         messages = [SystemMessage(system_str), HumanMessage(human_str)]
         output = self.chat_model(messages)
         return output.content
-    
+
+    def _load_chat_model(self, low_vram: bool, llm_config: dict) -> ChatOpenAI:
+        return ChatOpenAI(
+            openai_api_key=os.getenv("OPENAI_API_KEY"),
+            model_name=self.model_name,
+            temperature=llm_config.get('temperature', 0.8),
+        )
+
+class GPT4o(OpenAILLM):
+    model_name = LLMModels.GPT4o.value
+
+class GPT4oMini(OpenAILLM):
+    model_name = LLMModels.GPT4oMini.value
+
+class LLAMA31_8B(LLM):
+    pass
 
 if __name__ == "__main__":
-    llm = LLM(
-        model_id=LLMModels.GPT4o,
+    llm = GPT4o(
         low_vram=False,
         llm_config={'temperature': 0}
     )
