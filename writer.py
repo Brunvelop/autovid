666f4ff261b36eddce982fad2256c9855e3f1eb6
Brunvelop <brunvelop@gmail.com>
2024-09-06 16:21:17 +0200

    Add book writer

--- writer.py
+++ writer.py
@@ -1,4 +1,5 @@
 import json
+import textwrap
 from typing import List
 from pathlib import Path
 
@@ -14,7 +15,7 @@ class Writer():
 
     def generate_video_script(self, content: str, words_number: int = 100) -> str:
         text = self.llm.generate_text(
-                system_prompt= prompts.Prompts.ESP.GUION_VIDEO_VIRAL,
+                system_prompt= prompts.Prompts.ESP.GUION_SHORT_VIRAL,
                 system_examples= '', 
                 human_prompt= "Escribe el script para un video sobre {content}".format(content=content),
                 output_format = 
@@ -22,16 +23,6 @@ class Writer():
                     prompts.OutputFormats.ESP.SIN_SALTOS_DE_LINEA
         )
         return text
-    
-    def split_text(self, text: str) -> List[str]:
-        output = self.llm.generate_text(
-            system_prompt=prompts.OutputFormats.ENG.SPLIT_TEXT_SCENES,
-            system_examples='',
-            human_prompt="Transform this text: {text}. Return only an array [] do not write any additional text.".format(text=text),
-            output_format=''
-        )
-        text_splited_parsed = json.loads(output)
-        return text_splited_parsed
     
     def generate_storyboard(self, text: str) -> List[Scene]:
         output = self.llm.generate_text(
@@ -44,23 +35,57 @@ class Writer():
         storyboard = [Scene(text=scene['text'], image=scene['image']) for scene in storyboard_parsed]
         return storyboard
     
-    def save_storyboard(self, storyboard: List[Scene], filename: Path):
+    def save_script(self, script: str, filename: Path) -> None:
+        filename.parent.mkdir(parents=True, exist_ok=True)
+        filename.write_text(script, encoding='utf-8')
+
+    def save_storyboard(self, storyboard: List[Scene], filename: Path) -> None:
+        filename.parent.mkdir(parents=True, exist_ok=True)
         with open(filename, 'w', encoding='utf-8') as f:
             json.dump(storyboard, f, ensure_ascii=False, indent=2)
-
 
 class BookWriter(Writer):
     def __init__(self, llm: LLM) -> None:
         super().__init__(llm)
 
-    def generate_storyboard_from_chapter(self, filename: Path, batch_size: int = 10) -> List[Scene]:
+    def generate_storyboard_from_chapter(self, full_chapter_path: Path, batch_size: int = 10) -> List[Scene]:
         storyboard = []
         
-        for chunk in tqdm(list(self._chunk_file(filename, batch_size)), desc="Generating storyboard"):
+        for chunk in tqdm(list(self._chunk_file(full_chapter_path, batch_size)), desc="Generating storyboard"):
             chunk_storyboard = self.generate_storyboard(chunk)
             storyboard.extend(chunk_storyboard)
 
         return storyboard
+    
+    def generate_short_script_from_content(self, chapter_content: str, theme: str, words_number: int = 100) -> str:
+        text = self.llm.generate_text(
+            system_prompt=textwrap.dedent(f'''
+                Aqui tienes el capitulo de la iliada:
+                {chapter_content}
+                {prompts.Prompts.ESP.REGLAS_STORYTELLING}
+                {prompts.Prompts.ESP.PRESENTACION_PERSONAJES}
+            ''').strip(),
+            human_prompt=f'Escribe un video viral inspirado en este capitulo, enfocándote en el tema: {theme}',
+            output_format = 
+                prompts.OutputFormats.ESP.NUMERO_PALABRAS.format(words_number=words_number) + 
+                prompts.OutputFormats.ESP.SIN_SALTOS_DE_LINEA
+        )
+        return text
+    
+    def improve_text_storytelling(self, text: str, words_number: int = 100) -> str:
+        text_improved = self.llm.generate_text(
+            system_prompt=textwrap.dedent(f'''
+                Mejora este texto siguiendo las reglas de storytelling:
+                {text}
+                {prompts.Prompts.ESP.REGLAS_STORYTELLING}
+                {prompts.Prompts.ESP.PRESENTACION_PERSONAJES}
+            ''').strip(),
+            human_prompt='Escribe solo el texto mejorado, no des explicaciones',
+            output_format = 
+                prompts.OutputFormats.ESP.NUMERO_PALABRAS.format(words_number=words_number) + 
+                prompts.OutputFormats.ESP.SIN_SALTOS_DE_LINEA
+        )
+        return text_improved
 
     def _chunk_file(self, file_path: Path, chunk_size: int):
         with open(file_path, 'r', encoding='utf-8') as file:
@@ -68,15 +93,32 @@ class BookWriter(Writer):
             for i in range(0, len(lines), chunk_size):
                 yield ''.join(lines[i:i+chunk_size])
 
-
 if __name__ == "__main__":
     from LLM import GPT4o
     
     writer = BookWriter(
         llm=GPT4o(
             low_vram=False,
-            llm_config={'temperature': 0.8}
+            llm_config={'temperature': 0.5}
         )
     )
-    storyboard = writer.generate_storyboard_from_chapter(Path('data\HOMERO\LA_ILIADA\CAPITULO_001\CAPITULO_001.txt'))
-    writer.save_storyboard(storyboard, Path('data\HOMERO\LA_ILIADA\CAPITULO_001\CAPITULO_001_STORYBOARD.json'))
+
+    SHORT_N = 2
+    THEME = "La ira de Aquiles como tema central del poema."
+
+    CAPITULO1_FOLDER = Path('data/HOMERO/LA_ILIADA/CAPITULO_001')
+    SHORTS_FOLDER = (CAPITULO1_FOLDER / 'SHORTS')
+    CAPITULO1 = (CAPITULO1_FOLDER / 'CAPITULO_001.txt').read_text(encoding='utf-8')
+    
+    text = writer.generate_short_script_from_content(
+        chapter_content=CAPITULO1,
+        theme=THEME,
+        words_number=100
+    )
+    text_improved = writer.improve_text_storytelling(text, words_number=100)
+
+    print(f'{text} \n ->{text_improved} \n')
+
+    storyboard = writer.generate_storyboard(text_improved)
+    writer.save_storyboard(text_improved, (SHORTS_FOLDER / f'{SHORT_N}/text/script.txt'))
+    writer.save_storyboard(storyboard, (SHORTS_FOLDER / f'{SHORT_N}/text/storyboard.json'))
